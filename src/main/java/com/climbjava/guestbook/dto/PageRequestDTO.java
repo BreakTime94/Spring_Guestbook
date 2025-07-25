aa61d29574d08224e6791728facf8043e0b57072
Kimchan <kimchan9408@gmail.com>
2025-07-25 18:13:45 +0900

    250725 guestbook 페이징 처리(쿼리스트링 등) 마무리 및 종료

--- src/main/java/com/climbjava/guestbook/dto/PageRequestDTO.java
+++ src/main/java/com/climbjava/guestbook/dto/PageRequestDTO.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @Builder
 @AllArgsConstructor
@@ -23,5 +26,21 @@ public class PageRequestDTO {
 
   public Pageable getPageable(Sort sort) {
     return PageRequest.of(page - 1, size, sort); // 얘는 0부터 시작이므로 page에서 1을 뺀다.
+  }
+
+  public String getQs() {
+    UriComponentsBuilder builder = UriComponentsBuilder.fromPath("");
+    builder.queryParam("size", size );
+    builder.queryParam("type", type);
+    builder.queryParam("keyword", keyword);
+    return builder.build().toUriString();
+  }
+  public String getQs2() {
+    UriComponentsBuilder builder = UriComponentsBuilder.fromPath("");
+    builder.queryParam("page", page );
+    builder.queryParam("size", size );
+    builder.queryParam("type", type);
+    builder.queryParam("keyword", keyword);
+    return builder.build().toUriString();
   }
 }
